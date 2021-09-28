950801a16360c1e745ee142981528e604da7da3e
Tammy <tammyanndo@gmail.com>
2021-09-27 21:43:12 -0400

    added bodyParser and randomstring generator function

--- express_server.js
+++ express_server.js
@@ -4,10 +4,32 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  const chars ='abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = ' ';
+    const charLength = chars.length;
+    for ( let i = 0; i < 6; i++ ) {
+      result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+    return result;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
